901fb78232ddf64b2b687f0eb0f186ecc1e560c8
camelia assobhei <camelia.assobhei@gmail.com>
2023-08-29 16:07:47 +0200

    map avec current user et une balade

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "pages#home"
-  
+
   resources :dogs, only: %i[new create] do
-    resources :walk, only: %i[index show]
+    resources :walks, only: %i[index show]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
